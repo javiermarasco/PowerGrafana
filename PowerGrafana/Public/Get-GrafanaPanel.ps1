33e1913b9550eeb3a0de579f3c73a2c39d440d60
Javier Marasco <javier.marasco@gmail.com>
2021-04-05 10:13:03 +0200

    The Get-GrafanaPanel function should allow to retrieve by ids

--- PowerGrafana/Public/Get-GrafanaPanel.ps1
+++ PowerGrafana/Public/Get-GrafanaPanel.ps1
@@ -1,20 +1,34 @@
 function Get-GrafanaPanel {
 
     param (
-        [Parameter(Mandatory=$true)] [PSTypeName('PowerGrafana.Panel')] $Panel,
-        [Parameter(Mandatory=$true)] $Dashboard
+        [Parameter(Mandatory=$false)] [PSTypeName('PowerGrafana.Panel')] $Panel,
+        [Parameter(Mandatory=$false)] $PanelId,
+        [Parameter(Mandatory=$false)] $Dashboard,
+        [Parameter(Mandatory=$false)] $DashboardId
     )
 
     if (![string]::IsNullOrEmpty($Panel.id) -and ![string]::IsNullOrEmpty($Dashboard)  ) {
         $URI = Get-GrafanaURI
         $Header = New-GrafanaHeader
-        $RetrievedDashboard = Invoke-RestMethod -Method Get -Headers $Header -Uri $("$URI/api/dashboards/uid/" + $Dashboard.uid)
-        if ( $($RetrievedDashboard.dashboard.panels | where-object -property id -eq $Panel.id).count -gt 0) {
-            $PanelFound = $RetrievedDashboard.dashboard.panels | where-object -property id -eq $Panel.id
+        if (![string]::IsNullOrEmpty($Dashboard) ){
+            $RetrievedDashboard = Invoke-RestMethod -Method Get -Headers $Header -Uri $("$URI/api/dashboards/uid/" + $Dashboard.uid)
+            if ( $($RetrievedDashboard.dashboard.panels | where-object -property id -eq $Panel.id).count -gt 0) {
+                $PanelFound = $RetrievedDashboard.dashboard.panels | where-object -property id -eq $Panel.id
+            }
+            else {
+                $PanelFound = $null
+            }
+        }else{
+            $RetrievedDashboard = Invoke-RestMethod -Method Get -Headers $Header -Uri $("$URI/api/dashboards/uid/" + $DashboardId)
+            if ( $($RetrievedDashboard.dashboard.panels | where-object -property id -eq $PanelId).count -gt 0) {
+                $PanelFound = $RetrievedDashboard.dashboard.panels | where-object -property id -eq $PanelId
+            }
+            else {
+                $PanelFound = $null
+            }
         }
-        else {
-            $PanelFound = $null
-        }
+        
+        
         $PanelFound.PSObject.TypeNames.Insert(0, 'PowerGrafana.Panel')
         return $PanelFound
     }
