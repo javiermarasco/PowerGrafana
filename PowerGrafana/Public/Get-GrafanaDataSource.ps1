f655f607040196aee62d5787c8551de7e65fc74e
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:00:22 +0200

    Added help information

--- PowerGrafana/Public/Get-GrafanaDataSource.ps1
+++ PowerGrafana/Public/Get-GrafanaDataSource.ps1
@@ -1,3 +1,22 @@
+<#
+
+.SYNOPSIS
+Retrieves a datasource from Grafana by it's Id or a list of all datasources if no Id is provided.
+
+.DESCRIPTION
+By providing an Id as a parameter to this cmdlet you will get a datasource that can be later be used in targets, if you Id is provided it will return a list of datasources.
+
+.PARAMETER Id
+The DataSource id provided as a numeric value.
+
+.EXAMPLE
+Get-GrafanaDataSource -Id 2
+
+.NOTES
+
+.LINK
+
+#>
 function Get-GrafanaDataSource {
     param(
         [int]$Id
