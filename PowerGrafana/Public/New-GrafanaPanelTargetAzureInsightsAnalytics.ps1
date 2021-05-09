df05a9aa5a42191b03090c42397a3f1f550d1846
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:02:04 +0200

    Added help information

--- PowerGrafana/Public/New-GrafanaPanelTargetAzureInsightsAnalytics.ps1
+++ PowerGrafana/Public/New-GrafanaPanelTargetAzureInsightsAnalytics.ps1
@@ -1,3 +1,33 @@
+<#
+
+.SYNOPSIS
+Creates a target for Azure Insights Analytics
+
+.DESCRIPTION
+This cmdlet will create a target for Insights Analytics in a provided panel
+
+.PARAMETER query
+This is a string representing the query to execute in Insights Analytics.
+
+.PARAMETER resultFormat
+A string representing the format of the result returned by the query, this defaults to "time_series".
+
+.PARAMETER dataSourceName
+A string representing the name of the datasource that will provide the access to metrics for this target.
+
+.PARAMETER Panel
+A PowerGrafana.Panel where this target will be added.
+
+.PARAMETER Dashboard
+The dasboard where the panel containing this target is, this is represented with a PowerGrafana.Dashboard object.
+
+.EXAMPLE
+
+.NOTES
+
+.LINK
+
+#>
 function New-GrafanaPanelTargetAzureInsightsAnalytics {
     param (
         [string]$query = "",
