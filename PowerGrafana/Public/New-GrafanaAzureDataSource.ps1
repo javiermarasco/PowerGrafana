d97ad3b01ede98a10df1f09c47a9818bc677901c
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:00:56 +0200

    Added help information

--- PowerGrafana/Public/New-GrafanaAzureDataSource.ps1
+++ PowerGrafana/Public/New-GrafanaAzureDataSource.ps1
@@ -1,3 +1,51 @@
+<#
+
+.SYNOPSIS
+Creates an Azure datasource in Grafana
+
+.DESCRIPTION
+This cmdlet will create an Azure datasource in Grafana using the provided configuration.
+
+.PARAMETER name
+The name of the datasource to create
+
+.PARAMETER appInsightsAppId
+The application id of our Application Insights instance in Azure.
+
+.PARAMETER azureLogAnalyticsSameAs
+This is a flag to determine if the Log Analytics configuration is the same as the one used for Azure Monitor, setting this to True will allow to not provide a configuration for Log Analytics items.
+
+.PARAMETER clientId
+The client id of the Service Principal used in Azure to manage the resources.
+
+.PARAMETER cloudName
+
+
+.PARAMETER logAnalyticsClientId
+The client id of our log analytics workspace instance in Azure.
+
+.PARAMETER logAnalyticsClientSecret
+The secret of our log analytics workspace instance
+
+.PARAMETER logAnalyticsTenantId
+The tenant id of the Azure Active Directory where our log analytics workspace instance is deployed.
+
+.PARAMETER tenantId
+The tenant id where the Azure resources are deployed.
+
+.PARAMETER appInsightsApiKey
+The applications insights instrumentation key.
+
+.PARAMETER clientSecret
+The secret key of the Service Principal used to manage our resources in Azure.
+
+.EXAMPLE
+
+.NOTES
+
+.LINK
+
+#>
 function New-GrafanaAzureDataSource {
     param(
         [string]$name,
@@ -6,11 +54,11 @@ function New-GrafanaAzureDataSource {
         [string]$clientId,
         [string]$cloudName,
         [string]$logAnalyticsClientId,
+        [string]$logAnalyticsClientSecret,
         [string]$logAnalyticsTenantId,
         [string]$tenantId,
         [string]$appInsightsApiKey,
-        [string]$clientSecret,
-        [string]$logAnalyticsClientSecret
+        [string]$clientSecret
     )
     $URI = Get-GrafanaURI
     $Header = New-GrafanaHeader
