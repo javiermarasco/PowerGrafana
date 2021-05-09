6191900db71831186ebd0a672c58989c35ed617e
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:00:03 +0200

    Added help information

--- PowerGrafana/Public/Get-GrafanaDashboard.ps1
+++ PowerGrafana/Public/Get-GrafanaDashboard.ps1
@@ -1,16 +1,29 @@
+<#
+
+.SYNOPSIS
+Retrieve a Grafana dashboard from a dashboard id
+
+.DESCRIPTION
+This cmdlet will retrieve a Grafana dashboard represented as a PowerGrafana.Dashboard object that can be later be used to add panels to it.
+
+.PARAMETER DashboardId
+This is a number representing the Grafana dashboard id, this is also called "uid".
+
+.EXAMPLE
+Get-GrafanaDashboard -DashboardId usi13s
+
+.NOTES
+
+.LINK
+
+#>
 function Get-GrafanaDashboard {
     param (
-        [Parameter(Mandatory=$false)]$DashboardId,
-        [Parameter(Mandatory=$false)]$Dashboard
+        [Parameter(Mandatory=$false)]$DashboardId
     )
     $URI = Get-GrafanaURI
     $Header = New-GrafanaHeader
-    if (![string]::IsNullOrEmpty($Dashboard) ){
-        $RetrievedDashboard = $(Invoke-RestMethod -Method Get -Headers $Header -Uri $("$URI/api/dashboards/uid/" + $Dashboard.uid)).dashboard
-    }else{
-        $RetrievedDashboard = $(Invoke-RestMethod -Method Get -Headers $Header -Uri $("$URI/api/dashboards/uid/" + $DashboardId)).dashboard
-    }
-    
+    $RetrievedDashboard = $(Invoke-RestMethod -Method Get -Headers $Header -Uri $("$URI/api/dashboards/uid/" + $DashboardId)).dashboard
     $RetrievedDashboard.PSObject.TypeNames.Insert(0, 'PowerGrafana.Dashboard')
     return $RetrievedDashboard
 }
