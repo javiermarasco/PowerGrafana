2b161bdd819a98197c7bd9d01875c3afd8f9b230
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:01:52 +0200

    Added help information

--- PowerGrafana/Public/New-GrafanaPanelTargetAzureAppInsights.ps1
+++ PowerGrafana/Public/New-GrafanaPanelTargetAzureAppInsights.ps1
@@ -1,3 +1,42 @@
+<#
+
+.SYNOPSIS
+Creates a target for Azure Application Insights.
+
+.DESCRIPTION
+This cmdlet will create a target and add it to a provided panel, the letter representing the RefId of the target is automatically assigned starting with the letter "A" up to "Z", if more targets are added this cmdlet will return "Panel can't have more targets, maximum targets reached" in which case another panel needs to be created to add the targets.
+
+.PARAMETER dimension
+An array of strings defining the dimensions that we want to setup in this target.
+
+.PARAMETER metricName
+The name of the metric.
+
+.PARAMETER timeGrain
+A string representing the way the target will group the metrics, this defaults to "auto"
+
+.PARAMETER dataSourceName
+A string representing the name of the datasource that will provide the access to metrics for this target.
+
+.PARAMETER Panel
+A PowerGrafana.Panel where this target will be added.
+
+.PARAMETER Dashboard
+The dasboard where the panel containing this target is, this is represented with a PowerGrafana.Dashboard object.
+
+
+.EXAMPLE
+
+.NOTES
+
+.LINK
+New-GrafanaDashboard
+Get-GrafanaDashboard
+New-GrafanaPanel
+Get-GrafanaPanel
+Get-GrafanaDataSource
+
+#>
 function New-GrafanaPanelTargetAzureAppInsights {
     param (
         [string[]]$dimension = @(),
