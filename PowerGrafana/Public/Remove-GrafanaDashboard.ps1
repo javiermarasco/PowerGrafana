e19d2f7d95d6d964ea5eb4ee26f73358f16462e0
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:02:24 +0200

    Added help information

--- PowerGrafana/Public/Remove-GrafanaDashboard.ps1
+++ PowerGrafana/Public/Remove-GrafanaDashboard.ps1
@@ -1,3 +1,23 @@
+<#
+
+.SYNOPSIS
+Removes a Grafana dashboard.
+
+.DESCRIPTION
+This cmdlet can be used to delete a complete dashboard, it will delete all the other resources defined inside the dashboard such as panels, targets, alerts, etc.
+
+.PARAMETER Dashboard
+A PowerGrafana.Dashboard object that can be retrieved by "Get-GrafanaDashboard"
+
+.EXAMPLE
+Remove-GrafanaDashboard -Dashboard $MyGrafanaDashboard
+
+.NOTES
+
+.LINK
+Get-GrafanaDashboard
+
+#>
 function Remove-GrafanaDashboard {
     [CmdletBinding()]
     param (
