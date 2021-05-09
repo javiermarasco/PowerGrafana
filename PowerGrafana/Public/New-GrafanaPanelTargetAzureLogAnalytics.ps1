934a5872698a6fe64fecd2fc7565ef462c06b7fa
Javier Marasco <javier.marasco@gmail.com>
2021-05-09 10:02:16 +0200

    Added help information

--- PowerGrafana/Public/New-GrafanaPanelTargetAzureLogAnalytics.ps1
+++ PowerGrafana/Public/New-GrafanaPanelTargetAzureLogAnalytics.ps1
@@ -1,3 +1,36 @@
+<#
+
+.SYNOPSIS
+Creates a target to run queries to a Log Analytics Workspace.
+
+.DESCRIPTION
+This cmdlet will create a target to query a Log Analytics Workspace.
+
+.PARAMETER query
+This is a string representing the query to execute in the Log Analytics Workspace.
+
+.PARAMETER resultFormat
+A string representing the format of the result returned by the query, this defaults to "time_series".
+
+.PARAMETER workspace
+A string representing the workspace name of the Log Analytics workspace
+
+.PARAMETER dataSourceName
+A string representing the name of the datasource that will provide the access to metrics for this target.
+
+.PARAMETER Panel
+A PowerGrafana.Panel where this target will be added.
+
+.PARAMETER Dashboard
+The dasboard where the panel containing this target is, this is represented with a PowerGrafana.Dashboard object.
+
+.EXAMPLE
+
+.NOTES
+
+.LINK
+
+#>
 function New-GrafanaPanelTargetAzureLogAnalytics {
     param (
         [string]$query = "",
